0e707094db60c7c3362560fe2e6503f890f7f92b
Sebastian Schweigert <sebastian341@gmail.com>
2026-03-15 14:56:25 -0700

    Update flashcard for words (not generalized)

--- flashcard.py
+++ flashcard.py
@@ -12,20 +12,20 @@ HSK = 1
 seed = day_as_number
 
 def main():
-    chars, data_words, pinyin = load_dataset(HSK)
-    
-    values = [(word, val[1], val[0]) for word, val in pinyin.items()]
+    #chars, data_words, pinyin = load_dataset(HSK)
+    #
+    #values = [(word, val[1], val[0]) for word, val in pinyin.items()]
+
+    words = load_words(HSK)    
+    values = [(key, val) for key, val in words['hard'].items()]
 
     random.Random(seed).shuffle(values)
 
     for val in values:
         os.system('clear')
-        print(val[0])
-        input()
-        print(val[1])
-        input()
-        print(val[2])
-        input()
+        for elem in val:
+            print(elem)
+            input()
     
     
 
